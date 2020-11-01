32aaeb275b5768d1f504dec9459ab77e81baa1f5
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 18:42:31 -0500

    adding semicolon

--- DOMpractice.js
+++ DOMpractice.js
@@ -4,6 +4,6 @@ cont showMessageOne = () =>
 {
   document.querySelector("firstanswer").textContent = numOfTags;
   alert('There are ${numOfTags} li tags');
-}
+};
 const buttonOneElement = document.getElementById("buttonOne");
 buttonOneElement.addEventListener("click", showMessageOne);
