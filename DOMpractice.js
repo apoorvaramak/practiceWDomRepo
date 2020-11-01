2228b40c15a8e2fe3d4832d8a279666c26f9f933
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 18:11:29 -0500

    button one functionality

---
+++ DOMpractice.js
@@ -0,0 +1,9 @@
+const numOfTags = document.getElementsByTagName("li");
+
+cont showMessageOne = () =>
+{
+  document.querySelector("firstanswer").textContent = numOfTags;
+  alert('There are ${numOfTags} li tags'); 
+}
+const buttonOneElement = document.getElementById("buttonOne");
+buttonOneElement.addEventListener("click", showMessageOne)
