46663296e5f4233606e2bca3ca1d1beb717d4ce6
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 18:35:39 -0500

    updating button one function

--- DOMpractice.js
+++ DOMpractice.js
@@ -3,7 +3,7 @@ const numOfTags = document.getElementsByTagName("li");
 cont showMessageOne = () =>
 {
   document.querySelector("firstanswer").textContent = numOfTags;
-  alert('There are ${numOfTags} li tags'); 
+  alert('There are ${numOfTags} li tags');
 }
 const buttonOneElement = document.getElementById("buttonOne");
-buttonOneElement.addEventListener("click", showMessageOne)
+buttonOneElement.addEventListener("click", showMessageOne);
