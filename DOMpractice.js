93fb9adac3a7549e1c09f13df060d91a8811dfc8
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 22:35:32 -0500

    add functionality to second and third buttons

--- DOMpractice.js
+++ DOMpractice.js
@@ -2,8 +2,28 @@ const numOfTags = document.getElementsByTagName("li");
 
 const showMessageOne = () =>
 {
-  //document.querySelector("#firstanswer").textContent = numOfTags;
-  alert("There are" + numOfTags + "li tags");
+  document.querySelector("firstAnswer").textContent = numOfTags;
+  alert("There are " + numOfTags + " li tags");
 };
 const buttonOneElement = document.getElementById("buttonOne");
 buttonOneElement.addEventListener("click", showMessageOne);
+
+const numInDivOne = document.getElementsById("topThree");
+
+const showMessageTwo = () =>{
+  document.querySelector("secondAnswer").textContent = numInDivOne;
+  alert("There are " + numInDivOne + " elements in the first div");
+};
+
+const buttonTwoElement = document.getElementById("buttonTwo");
+buttonTwoElement.addEventListener("click", showMessageTwo);
+
+const numInDivTwo = document.getElementsById("topTen");
+
+const showMessageThree = () => {
+  document.querySelector("thirdAnswer").textContent = numInDivTwo;
+  alert("There are " + numInDivTwo + " elements in the second div");
+};
+
+const buttonThreeElement = document.getElementById("buttonThree");
+buttonThreeElement.addEventListener("click", showMessageThree);
