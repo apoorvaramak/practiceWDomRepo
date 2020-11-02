7e9b8123d08934cb9d74c28aa2086c5cc003e44c
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 23:47:24 -0500

    adding li

--- DOMpractice.js
+++ DOMpractice.js
@@ -8,21 +8,21 @@ const showMessageOne = () =>
 const buttonOneElement = document.getElementById("buttonOne");
 buttonOneElement.addEventListener("click", showMessageOne);
 
-const numInDivOne = document.getElementById("topThree");
+const numInDivOne = document.getElementById("topThree").getElementsByTagName("li");
 
 const showMessageTwo = () =>{
-  document.querySelector("#secondAnswer").textContent = numInDivOne.innerHTML.length;
-  alert("There are " + numInDivOne.innerHTML.length + " elements in the first div");
+  document.querySelector("#secondAnswer").textContent = numInDivOne.length;
+  alert("There are " + numInDivOne.length + " elements in the first div");
 };
 
 const buttonTwoElement = document.getElementById("buttonTwo");
 buttonTwoElement.addEventListener("click", showMessageTwo);
 
-const numInDivTwo = document.getElementById("topTen");
+const numInDivTwo = document.getElementById("topTen").getElementsByTagName("li");
 
 const showMessageThree = () => {
-  document.querySelector("#thirdAnswer").textContent = numInDivTwo.innerHTML.length;
-  alert("There are " + numInDivTwo.innerHTML.length + " elements in the second div");
+  document.querySelector("#thirdAnswer").textContent = numInDivTwo.length;
+  alert("There are " + numInDivTwo.length + " elements in the second div");
 };
 
 const buttonThreeElement = document.getElementById("buttonThree");
