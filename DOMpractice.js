b078034f02b5c1f1897bbec716fb1f5a2c32cdfd
Apoorva Ramakrishnan <apoorvar@comcast.net>
2020-11-01 22:58:22 -0500

    debugging

--- DOMpractice.js
+++ DOMpractice.js
@@ -1,12 +1,12 @@
 const numOfTags = document.getElementsByTagName("li");
 
-const showMessageOne = () =>
-{
-  document.querySelector("#firstAnswer").textContent = numOfTags.length;
-  alert("There are " + numOfTags + " li tags");
-};
-const buttonOneElement = document.getElementById("buttonOne");
-buttonOneElement.addEventListener("click", showMessageOne);
+// const showMessageOne = () =>
+// {
+//   document.querySelector("#firstAnswer").textContent = numOfTags.length;
+//   alert("There are " + numOfTags + " li tags");
+// };
+// const buttonOneElement = document.getElementById("buttonOne");
+// buttonOneElement.addEventListener("click", showMessageOne);
 
 const numInDivOne = document.getElementById("topThree");
 
